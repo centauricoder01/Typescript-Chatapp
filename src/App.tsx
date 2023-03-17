8f6d16ef2af4760bd7798d700b58b579d9daaebf
Rajendra <itsrajendrapatel@gmail.com>
2023-03-17 14:01:03 +0530

    Serve side Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
 function App() {
-  return (
-    <div className="App">
-      <p className="mt-20 bg-slate-500">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quam
-        quia quisquam odit inventore dicta, ipsa harum beatae fuga officiis
-        accusantium dolorem numquam id libero voluptatem, impedit mollitia qui
-        pariatur.
-      </p>
-      <h1 className="text-[140px]">Hello world</h1>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
